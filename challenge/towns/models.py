ca86dc34bec81c3aa5ae880647ce410daf430cfa
hds <haile.dossantos@gmail.com>
2017-11-02 00:13:47 +0100

    model creation

--- challenge/towns/models.py
+++ challenge/towns/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Town(models.Model):
+    region_code = models.IntegerField()
+    region_name = models.CharField(max_length=100)
+    department_code = models.IntegerField()
+    district_code = models.IntegerField()
+    town_code = models.IntegerField(primary_key=True)
+    town_name = models.CharField(max_length=100)
+    population = models.IntegerField()
