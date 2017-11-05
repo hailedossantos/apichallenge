79cf5c4d32963c5962aaa0fe036e88a6eb4c0f34
hds <haile.dossantos@gmail.com>
2017-11-05 23:37:09 +0100

    JSON response with sample data

--- challenge/towns/views.py
+++ challenge/towns/views.py
@@ -4,6 +4,32 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from .models import Town
+
 def getTowns(request):
-    data = {'name': 'Town1'}
-    return JsonResponse(data)
+    town1 = Town(region_code = 53,
+                region_name = "Bretagne",
+                department_code = 22,
+                district_code = 2,
+                town_code = 137,
+                town_name = "Maël-Carhaix",
+                population = 1.580)
+    town1.save()
+    town2 = Town(region_code = 11,
+                region_name = "Île-de-France",
+                department_code = 93,
+                district_code = 3,
+                town_code = 66,
+                town_name = "Saint-Denis",
+                population = 111.752)
+    town2.save()
+    town3 = Town(region_code = 11,
+                region_name = "Île-de-France",
+                department_code = 75,
+                district_code = 1,
+                town_code = 108,
+                town_name = "Paris 8e  Arrondissement",
+                population = 38.902)
+    town3.save()
+    towns = Town.objects.values()
+    return JsonResponse(dict(towns_list = list(towns)))
