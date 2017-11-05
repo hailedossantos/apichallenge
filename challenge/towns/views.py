cd48503ce913bcbae688cfe662b742c4b400c1ec
hds <haile.dossantos@gmail.com>
2017-11-05 20:32:53 +0100

    first JSON response

--- challenge/towns/views.py
+++ challenge/towns/views.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def getTowns(request):
+    data = {'name': 'Town1'}
+    return JsonResponse(data)
