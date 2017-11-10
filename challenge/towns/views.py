40801017f91baff9cbcb6a8fab3be02dc42c282a
hds <haile.dossantos@gmail.com>
2017-11-10 05:29:05 +0100

    filtering and ordering

--- challenge/towns/views.py
+++ challenge/towns/views.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
+from rest_framework.filters import OrderingFilter
+
+from django_filters import rest_framework as filters
 
 from .models import Town
 from .serializers import TownSerializer
 
-import json
+class TownFilter(filters.FilterSet):
+    class Meta:
+        model = Town
+        fields = {
+            'population': ['lt', 'gt'],
+            'department_code': ['exact', 'lt', 'gt'],
+            'town_name': ['icontains'],
+        }
 
 class TownsView(ListAPIView):
     queryset = Town.objects.all()
     serializer_class = TownSerializer
+    filter_backends = (OrderingFilter, filters.DjangoFilterBackend,)
+    filter_class = TownFilter
 
     def get(self, request, *args, **kwargs):        
         return self.list(request, *args, **kwargs)    
