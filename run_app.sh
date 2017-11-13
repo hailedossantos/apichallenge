933ef66654295cfb1c4fd0f41761be49c81a9d62
hds <haile.dossantos@gmail.com>
2017-11-13 02:30:56 +0100

    startup script fix

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python challenge/manage.py migrate
-python challenge/manage.py towns_data.json
+python challenge/manage.py loaddata towns_data.json
 python challenge/manage.py runserver
